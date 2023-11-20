07fcd7360db46c2d7f17d93925e27d1571518216
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-20 11:40:11 +0100

    placeholder in

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 		<p className="normal-text spacing">This is a normal text.</p>
 		<p className="faded-text spacing">This is a faded text.</p>
 
-		<p className="ball-001 spacing"></p>
+		<p className="ball-001 shadow spacing"></p>
 		<p className="ball-002 shadow spacing"></p>
 		<p className="ball-003 spacing"></p>
 		<p className="ball-004 spacing"></p>
