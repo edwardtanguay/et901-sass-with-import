556adf96bd40d5624a819f7e3e3cd6f83da5f2f7
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-20 11:03:56 +0100

    test style for mixins

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './sass/main.scss';
 
 function App() {
-  return (
-    <>
-    <h1>Sass with @import</h1>
-    <p>testing</p>
-    </>
-  )
+	return (
+		<>
+		<h1>Sass with @import</h1>
+		<p className="error-text spacing">This is an error.</p>
+		<p className="info-text spacing">This is an info.</p>
+		<p className="normal-text spacing">This is a normal text.</p>
+		<p className="faded-text spacing">This is a faded text.</p>
+		</>
+	)
 }
 
 export default App
