54854c878087f68a123ea59462532bbf5c871dfc
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-20 11:35:38 +0100

    first ball

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ function App() {
 	return (
 		<>
 		<h1>Sass with @import</h1>
+
 		<p className="error-text spacing">This is an error.</p>
 		<p className="info-text spacing">This is an info.</p>
 		<p className="normal-text spacing">This is a normal text.</p>
 		<p className="faded-text spacing">This is a faded text.</p>
+
+		<p className="ball-001 spacing"></p>
+		<p className="ball-002 shadow spacing"></p>
+		<p className="ball-003 spacing"></p>
+		<p className="ball-004 spacing"></p>
 		</>
 	)
 }
